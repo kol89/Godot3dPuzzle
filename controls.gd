0753144eee45e4212b8ed79e4e9b2e232213c691
kol89 <afominovs@gmail.com>
2026-09-10 17:19:10 +0300

    Automatic commit by Aleksandr Fominov

--- controls.gd
+++ controls.gd
@@ -1,31 +1,12 @@
 extends Node
 @onready var maze: RigidBody3D = $"."
-var rotation_limit = 0.5
+const rotation_limit = 0.5
+const rotation_speed = 1
+var RotX: float
+var RotZ: float
+var snap = 1.2
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-#func _input(event):
-	#if event.is_action_pressed("left"):
-		#maze.rotatex(10)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("left"):
-		maze.rotate_z(0.02)
-	elif Input.is_action_pressed("right"):
-		maze.rotate_z(-0.02)
-	elif Input.is_action_pressed("up"):
-		maze.rotate_x(-0.02)
-	elif Input.is_action_pressed("down"):
-		maze.rotate_x(0.02)
-	else:
-		maze.rotation /= Vector3(1.1,1.1,1.1)
-		if maze.rotation<=Vector3(0.001, 0.001, 0.001):
-			maze.rotation = Vector3(0,0,0)
-		
-	print(maze.rotation_degrees.x,"", maze.rotation_degrees.z)
+func rotationLimiter() -> void:
 	if maze.rotation.x > rotation_limit:
 		maze.rotation.x = rotation_limit
 	if maze.rotation.x < -rotation_limit:
@@ -34,4 +15,31 @@ func _process(delta: float) -> void:
 		maze.rotation.z = rotation_limit
 	if maze.rotation.z < -rotation_limit:
 		maze.rotation.z = -rotation_limit
-		
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _unhandled_key_input(event):
+	if !event.is_pressed(): 
+		RotX = 0
+		RotZ = 0
+		print(snap)
+		maze.rotation.x /= snap
+		maze.rotation.z /= snap
+		if maze.rotation<=Vector3(0.0001, 0.0001, 0.0001) and maze.rotation>=Vector3(-0.0001, -0.0001, -0.0001):
+			maze.rotation = Vector3(0,0,0)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		maze.rotation += get_process_delta_time() * Vector3(0,0,rotation_speed)
+	if Input.is_action_pressed("right"):
+		maze.rotation += get_process_delta_time() * Vector3(0,0,-rotation_speed)
+	if Input.is_action_pressed("up"):
+		maze.rotation += get_process_delta_time() * Vector3(-rotation_speed,0,0)
+	if Input.is_action_pressed("down"):
+		maze.rotation += get_process_delta_time() * Vector3(rotation_speed,0,0)
+	print(maze.rotation_degrees.x,"", maze.rotation_degrees.z)
+	rotationLimiter()
+	
